e64d9cd50ba2ba5bcb1a6522d8ce6eba2dc80327
Jason Chan <jchan321738@gmail.com>
2021-04-09 16:49:57 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,15 @@
+# Rock Paper Scissors
+
+Want to practice your rock, paper, scissors skills? 
+
+## Technology
+
+* Javascript
+* HTML
+
+## Launch
+
+Click on the link below! It will prompt you two questions. Simply answer the questions and follow the instructions to reveal the answer!
+
+Link: [Magic Eight Ball](http://127.0.0.1:5500/index.html "Magic Eight Ball")
+
---
+++ rock-paper-scissors.js
@@ -0,0 +1,62 @@
+const getUserChoice = (userInput) => {
+    userInput = prompt("Choose rock, paper or scissors");
+    userInput = userInput.toLowerCase();
+    if (userInput === 'rock' || userInput === 'paper' || userInput === 'scissors') {
+      return userInput;
+    } else {
+      console.log('Please only choose rock, paper or scissors!');
+      getUserChoice()
+    };
+  }; 
+  
+  const getComputerChoice = () => {
+    randomNumber = Math.floor(Math.random() * 3) 
+      switch (randomNumber) {
+        case 0:
+          return 'rock';
+          break;
+        case 1:
+          return 'paper';
+          break;
+        case 2:
+          return 'scissors';
+          break;
+      };
+    };
+  
+  const determineWinner = (userChoice, computerChoice) => {
+    if (userChoice === computerChoice) {
+      return 'Draw'
+    }
+    if (userChoice === 'rock'){
+      if (computerChoice === 'paper'){
+        return 'Computer won!'
+      } else {
+        return 'User won!'
+      }
+    };
+    if (userChoice === 'paper'){
+      if (computerChoice === 'scissors') {
+        return 'Computer won!'
+      } else {
+        return 'User won!'
+      }
+    };
+    if (userChoice === 'scissors'){
+      if (computerChoice === 'rock') {
+        return 'Computer won!'
+      } else {
+        return 'User won!'
+      }
+    };
+  };
+  
+  const playGame = () => {
+    let userChoice = getUserChoice();
+    let computerChoice = getComputerChoice();
+    console.log(`You chose ${userChoice} and the computer chose ${computerChoice}`);
+    console.log(determineWinner(userChoice, computerChoice));
+  };
+  
+  playGame()
+  
